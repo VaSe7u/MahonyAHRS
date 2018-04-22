de6ed7e0640fa4baa2a7ff2b3beaa371cd16af58
Vasil Kalchev <vase7u@gmail.com>
2018-04-22 05:29:33 +0300

    .

--- src/MahonyAHRS.cpp
+++ src/MahonyAHRS.cpp
@@ -9,9 +9,9 @@
 // provided under the GNU General Public Licence unless an alternative licence
 // is provided in source."
 //
-// Date     Author      Notes
-// 29/09/2011 SOH Madgwick    Initial release
-// 02/10/2011 SOH Madgwick    Optimised for reduced CPU load
+// Date          Author          Notes
+// 29/09/2011    SOH Madgwick    Initial release
+// 02/10/2011    SOH Madgwick    Optimised for reduced CPU load
 //
 // Algorithm paper:
 // http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=4608934&url=http%3A%2F%2Fieeexplore.ieee.org%2Fstamp%2Fstamp.jsp%3Ftp%3D%26arnumber%3D4608934
