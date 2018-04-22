5fa9569501ca72a8f01cae6825fa3a2387c6a370
Vasil Kalchev <vase7u@gmail.com>
2018-04-22 05:18:27 +0300

    Interface change.

--- src/MahonyAHRS.h
+++ src/MahonyAHRS.h
@@ -15,46 +15,28 @@
 #include <math.h>
 
 class Mahony {
-private:
-	float twoKp;		// 2 * proportional gain (Kp)
-	float twoKi;		// 2 * integral gain (Ki)
-	float q0, q1, q2, q3;	// quaternion of sensor frame relative to auxiliary frame
-	float integralFBx, integralFBy, integralFBz;  // integral error terms scaled by Ki
-	float invSampleFreq;
-	float roll, pitch, yaw;
-	char anglesComputed;
-	static float invSqrt(float x);
-	void computeAngles();
-
-
 public:
-	Mahony();
-	void begin(float sampleFrequency) { invSampleFreq = 1.0f / sampleFrequency; }
-	void update(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
-	void updateIMU(float gx, float gy, float gz, float ax, float ay, float az);
-	float getRoll() {
-		if (!anglesComputed) computeAngles();
-		return roll * 57.29578f;
-	}
-	float getPitch() {
-		if (!anglesComputed) computeAngles();
-		return pitch * 57.29578f;
-	}
-	float getYaw() {
-		if (!anglesComputed) computeAngles();
-		return yaw * 57.29578f + 180.0f;
-	}
-	float getRollRadians() {
-		if (!anglesComputed) computeAngles();
-		return roll;
-	}
-	float getPitchRadians() {
-		if (!anglesComputed) computeAngles();
-		return pitch;
-	}
-	float getYawRadians() {
-		if (!anglesComputed) computeAngles();
-		return yaw;
-	}
-};
+  Mahony(uint32_t updatePeriod);
+  void setP(float p);
+  void setI(float i);
 
+  void update(float &yaw, float &pitch, float &roll,
+              float ax, float ay, float az,
+              float gx, float gy, float gz);
+
+  void update(float &yaw, float &pitch, float &roll,
+              float ax, float ay, float az,
+              float gx, float gy, float gz,
+              float mx, float my, float mz);
+
+private:
+  float _twoKp = 2.0f * 0.5f;
+  float _twoKi = 2.0f * 0.0f;
+  float _quaternion[4] = { 1.0f, 0.0f, 0.0f, 0.0f };
+  float _integralFBx = 0.0f;
+  float _integralFBy = 0.0f;
+  float _integralFBz = 0.0f;
+  float _invSampleFreq;
+  static float _invSqrt(float x);
+  void _toYawPitchRoll(float &yaw, float &pitch, float &roll);
+};
