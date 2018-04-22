9fd4a318f73afef8588efe64cd31ae39137e87ca
Vasil Kalchev <vase7u@gmail.com>
2018-04-22 05:18:08 +0300

    Style change

--- src/MahonyAHRS.h
+++ src/MahonyAHRS.h
@@ -5,17 +5,14 @@
 // Madgwick's implementation of Mayhony's AHRS algorithm.
 // See: http://www.x-io.co.uk/open-source-imu-and-ahrs-algorithms/
 //
-// Date			Author			Notes
-// 29/09/2011	SOH Madgwick    Initial release
-// 02/10/2011	SOH Madgwick	Optimised for reduced CPU load
+// Date     Author      Notes
+// 29/09/2011 SOH Madgwick    Initial release
+// 02/10/2011 SOH Madgwick    Optimised for reduced CPU load
 //
 //=============================================================================================
-#ifndef MahonyAHRS_h
-#define MahonyAHRS_h
+#pragma once
+#include <stdint.h>
 #include <math.h>
-
-//--------------------------------------------------------------------------------------------
-// Variable declaration
 
 class Mahony {
 private:
@@ -29,8 +26,6 @@ private:
 	static float invSqrt(float x);
 	void computeAngles();
 
-//-------------------------------------------------------------------------------------------
-// Function declarations
 
 public:
 	Mahony();
@@ -63,4 +58,3 @@ public:
 	}
 };
 
-#endif
