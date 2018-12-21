d54321877b644cc41437e5a2a02da2055c915fe8
Vasil Kalchev <vase7u@gmail.com>
2018-12-21 20:13:04 +0200

    Changed the periodic update implementation.

--- examples/NineAxisMahony/NineAxisMahony.ino
+++ examples/NineAxisMahony/NineAxisMahony.ino
@@ -20,8 +20,16 @@
 //! YOUR_IMU your_imu(your_imu_params...);
 
 // The period between calls to Mahony's update function.
-const float mahonySamplePeriod = 0.05f; // in seconds
-Mahony mahony(mahonySamplePeriod);
+const float mahonySamplePeriodS = 0.05f; // in seconds
+const unsigned short mahonySamplePeriodMs = 50; // in milliseconds
+Mahony mahony(mahonySamplePeriodS);
+
+bool timeToUpdateMahony();
+
+/* Assign this function pointer to a function that returns the time
+   since boot in milliseconds. Currently points to Arduino's millis()
+   function. */
+//! unsigned long (*milliseconds_ptr)(void) = &millis;
 
 void setup() {
 //! your_imu.initialize(your_imu_init_params...);
@@ -63,9 +71,7 @@ void loop() {
 
   /* The Mahony algorithm must be called at fixed period specified
      earlier in the constructor. In this case 50 milliseconds. */
-  static unsigned long mahonyLMS = millis();
-  if (millis() - mahonyLMS > 50) {
-    mahonyLMS = millis();
+  if (timeToUpdateMahony()) {
     /* The update function implements the Mahony's sensor fusion
        algorithm. The first three parameters are references to the
        yaw, pitch and roll variables, where the result will be
@@ -84,4 +90,14 @@ void loop() {
   float pitchDeg = pitchRad * 57.295779513f;
   float rollDeg = rollRad * 57.295779513f;
 
+}
+
+bool timeToUpdateMahony() {
+  static unsigned long mahonyLMS = (*milliseconds_ptr)();
+  if ((*milliseconds_ptr)() - mahonyLMS > mahonySamplePeriodMs) {
+    mahonyLMS = (*milliseconds_ptr)();
+    return true;
+  } else {
+    return false;
+  }
 }
